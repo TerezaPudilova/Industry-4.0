04104277b042a43a17d565624f80322d4195e52b
Tereza Pudilová <tery.pudilova@gmail.com>
2020-11-28 14:38:04 +0100

    logo png

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
 
-
-export const Menu=()=>{
-
-  return(
+export const Menu = () => {
+  return (
     <>
-    <nav className="menu">
-      <div className="navigation"></div>
-      <div className="logoFBE">
-        <img id="logo" src="./img/logoFBE.jpg" alt="logoFBE"/>
-      </div>
-      <div className="links">
-        <Link className="link" to="/dotaznik" >Questionnnaire</Link>
-        <Link className="link" to="/finalPage" >Finalpage</Link>
-        <a className="link" href="#">FBE Praha</a>
-        <a className="link" href="#">FBE Bratislava</a>
-      </div>
-    </nav>
-  </>
-); 
-}
+      <nav className="menu">
+        <div className="navigation"></div>
+        <div className="logoFBE">
+          <img id="logo" src={logo} alt="logoFBE" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/dotaznik">
+            Questionnnaire
+          </Link>
+          <Link className="link" to="/finalPage">
+            Finalpage
+          </Link>
+          <a className="link" href="#">
+            FBE Praha
+          </a>
+          <a className="link" href="#">
+            FBE Bratislava
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
