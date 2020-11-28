2655e3503037f54fbdcb0f4f2cfec50e9f1ed3c6
Tereza Pudilová <tery.pudilova@gmail.com>
2020-11-28 15:16:00 +0100

    menu container

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,30 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
 import logo from '../img/logo.png';
+import { Wrapper } from './menu.styles';
+import { Container } from '../styles/Container';
 
 export const Menu = () => {
   return (
     <>
-      <nav className="menu">
-        <div className="navigation"></div>
-        <div className="logoFBE">
-          <img id="logo" src={logo} alt="logoFBE" />
-        </div>
-        <div className="links">
-          <Link className="link" to="/dotaznik">
-            Questionnnaire
-          </Link>
-          <Link className="link" to="/finalPage">
-            Finalpage
-          </Link>
-          <a className="link" href="#">
-            FBE Praha
-          </a>
-          <a className="link" href="#">
-            FBE Bratislava
-          </a>
-        </div>
-      </nav>
+      <Wrapper>
+        <Container>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={2}>
+                <div className="logoFBE">
+                  <img src={logo} alt="logoFBE" width="150" height="60" />
+                </div>
+              </Grid.Column>
+              <Grid.Column width={14}>
+                <div className="links">
+                  <Link className="link" to="/dotaznik">
+                    Questionnnaire
+                  </Link>
+                  <Link className="link" to="/finalPage">
+                    Finalpage
+                  </Link>
+                  <a className="link" href="#">
+                    FBE Praha
+                  </a>
+                  <a className="link" href="#">
+                    FBE Bratislava
+                  </a>
+                </div>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Wrapper>
     </>
   );
 };
---
+++ src/components/menu.styles.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.nav`
+  background-color: white;
+  position: fixed;
+  padding: 16px 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
